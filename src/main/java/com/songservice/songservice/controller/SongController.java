9258e7b157fa080a9585618144cd8c3c9ad939a1
Ankit Goel <vineet.battula@cognizant.com>
2019-04-12 17:02:18 -0400

    song services

--- src/main/java/com/songservice/songservice/controller/SongController.java
+++ src/main/java/com/songservice/songservice/controller/SongController.java
@@ -3,14 +3,20 @@ package com.songservice.songservice.controller;
 import com.songservice.songservice.models.Song;
 import com.songservice.songservice.service.SongService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
 @RestController
 public class SongController {
 
+
     SongService songService;
+
+    @Value("${songLimit}")
+    private int songLimit;
 
     @Autowired
     public SongController(SongService songService){
@@ -24,15 +30,10 @@ public class SongController {
 
     }
 
-//
-//    @GetMapping("/songs")
-//    public List<Song> listSong() {
-//        return songService.retrieveSongs();
-//    }
-
 
     @GetMapping("/songs/{title}")
     public List<Song> getSongsByTitle(@PathVariable String title){
+        System.out.println("song limit + "+songLimit);
         return songService.retrieveSongsByTitle(title);
     }
 
@@ -40,6 +41,7 @@ public class SongController {
     @GetMapping("/songs/artist/{artist}")
     public List<Song> getSongsByArtist(@PathVariable String artist){
         return songService.getSongByArtist(artist);
+
     }
 
 
